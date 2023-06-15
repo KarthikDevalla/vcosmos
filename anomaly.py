9db5b28b4bb4e90214fa2ea557cb8bb51a9397f8
Karthik Devalla <78099144+KarthikDevalla@users.noreply.github.com>
2023-06-15 11:42:21 -0400

    Update anomaly.py

--- anomaly.py
+++ anomaly.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 
 class O_Seive:
 	def __init__(self, data, column, tsf=1, bsf=1):
-
+	
 		if len(inspect.signature(O_Seive.__init__).parameters)-1 !=4:
 			raise TypeError('Invalid number of arguments')
-
+	
 		self.column=column # Name of the column
 		self.data=data # Dataframe
 		self.tsf=tsf # Top scaling factor.
@@ -26,7 +26,7 @@ class O_Seive:
 		self.ymin=self.data[self.column].min()
 		self.ymax=self.data[self.column].max()
 		self.zval=np.mean(np.square(self.data[self.column]))
-
+	
 		print('Filtering Initiated....')
 	
 	def hcps_plot(self):
@@ -41,9 +41,9 @@ class O_Seive:
 		z_dist_plane1, z_dist_plane2=self.learning()
 		ax.plot_surface([[self.xmin, self.xmax], [self.xmin, self.xmax]], [[self.ymin, self.ymin], [self.ymax, self.ymax]], np.array([[z_dist_plane1, z_dist_plane1], [z_dist_plane1, z_dist_plane1]]), alpha=0.5,color='red')
 		ax.plot_surface([[self.xmin, self.xmax], [self.xmin, self.xmax]], [[self.ymin, self.ymin], [self.ymax, self.ymax]], np.array([[z_dist_plane2, z_dist_plane2], [z_dist_plane2, z_dist_plane2]]), alpha=0.5,color='red')
-
+	
 		return plt.show()
-
+	
 	def learning(self):
 		centre = np.array([np.mean(self.data.index), np.mean(self.data[self.column]),self.zval])
 		top_distances, bottom_distances =[],[]
@@ -83,14 +83,14 @@ class O_Seive:
 			lower_point=np.array([l_x,l_y,(l_z[0]+l_z[1])/2])
 		else:
 			lower_point=np.array([l_x,l_y,l_z[0]])
-
+	
 		lower_distribution_length = np.linalg.norm(lower_point-centre)
-
+	
 		z_dist_plane1= self.zval + (upper_distribution_length*self.tsf) 
 		z_dist_plane2= self.zval - (lower_distribution_length*self.bsf)
-
+	
 		return z_dist_plane1, z_dist_plane2
-
+	
 	def encompassing_points(self,z,d1,d2):
 		if (z > d1)  or (z < d2):
 			return 0 # OUTLIER
@@ -105,8 +105,8 @@ class O_Seive:
 		print('Filtering Complete.')
 		print('Ouliers Removed:',self.data.shape[0]-new_data.shape[0])
 		return new_data 
-
-
-
-
-
+	
+	
+	
+	
+	
